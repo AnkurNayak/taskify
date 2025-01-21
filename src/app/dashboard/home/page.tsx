246b130d2c78ddc2e2653221e8f8fed872e59083
AnkurNayak <ankurzac3@gmail.com>
2025-01-21 12:34:16 +0530

    issue_resolve:y

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -8,25 +8,21 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col w-full max-w-screen-xl mx-auto px-6 sm:px-8">
-      <div className="flex flex-col flex-auto items-center min-w-0 my-8 sm:my-12">
-        <div className="flex flex-auto items-center min-w-0">
-          <div className="flex flex-auto items-center min-w-0">
-            <div className="flex-0 w-16 h-16 rounded-full overflow-hidden">
-              <Image
-                src={session?.user?.image as string}
-                alt={session?.user?.name as string}
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="flex flex-col min-w-0 ml-4">
-              <div className="text-2xl lg:text-5xl font-semibold tracking-tight truncate">
-                Welcome Back, {session?.user?.name}
-              </div>
-            </div>
+      <div className="flex flex-col md:flex-row items-center my-12">
+        <div className="w-16 h-16 rounded-full overflow-hidden">
+          <Image
+            src={session?.user?.image as string}
+            alt={session?.user?.name as string}
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="flex flex-col min-w-0 ml-4">
+          <div className="text-2xl lg:text-4xl font-semibold">
+            Welcome Back, {session?.user?.name}
           </div>
         </div>
-        <Button className="mt-8 max-w-fit" onClick={() => signOut()}>
+        <Button className="max-w-fit md:ml-auto" onClick={() => signOut()}>
           Sign Out
         </Button>
       </div>
